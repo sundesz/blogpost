66f9ee752ace286c1887d62ee16eb6bfa367f9e3
Sandesh Hyoju <sundesz@gmail.com>
2023-02-12 00:52:38 +0200

    Refactor 404 page

--- client/src/components/Page404.tsx
+++ client/src/components/Page404.tsx
@@ -1,15 +1,11 @@
 import { Container } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import GoBack from './GoBack';
 
 const Page404 = () => {
-  const navigate = useNavigate();
-
   return (
     <Container className="p-5">
       <div className="display-1 fw-bold text-center">Page not found.</div>
-      <div onClick={() => navigate(-1)} className="go-back">
-        &#8249; go back
-      </div>
+      <GoBack />
     </Container>
   );
 };
