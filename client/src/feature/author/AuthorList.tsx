eeed355b05ecbf8dab6173c0d8a199eac29a067d
Sandesh Hyoju <sundesz@gmail.com>
2023-07-13 12:20:11 +0300

    Add render function

--- client/src/feature/author/AuthorList.tsx
+++ client/src/feature/author/AuthorList.tsx
@@ -7,30 +7,34 @@ interface AuthorListProps {
   authors: Author[];
 }
 
+const renderAuthor = (authors: Author[]) => {
+  if (authors.length <= 0) {
+    return <div className="no-data">No author yet.</div>;
+  }
+
+  return authors.map((author) => (
+    <Card key={author.userId}>
+      <AuthorImage author={author} />
+
+      <Card.Body>
+        <Card.Title>
+          <Link to={`/authors/${author.userId}`}>{author.name}</Link>
+        </Card.Title>
+        <Card.Body>{author.email}</Card.Body>
+        <Card.Footer>
+          <Link to={`/authors/${author.userId}`}>
+            {author.blogCount ? author.blogCount : 0} blog(s)
+          </Link>
+        </Card.Footer>
+      </Card.Body>
+    </Card>
+  ));
+};
+
 const AuthorList = ({ authors }: AuthorListProps) => {
   return (
     <Container className="author-container py-5">
-      {authors && authors.length ? (
-        authors.map((author) => (
-          <Card key={author.userId}>
-            <AuthorImage author={author} />
-
-            <Card.Body>
-              <Card.Title>
-                <Link to={`/authors/${author.userId}`}>{author.name}</Link>
-              </Card.Title>
-              <Card.Body>{author.email}</Card.Body>
-              <Card.Footer>
-                <Link to={`/authors/${author.userId}`}>
-                  {author.blogCount ? author.blogCount : 0} blog(s)
-                </Link>
-              </Card.Footer>
-            </Card.Body>
-          </Card>
-        ))
-      ) : (
-        <div className="no-data">No author yet.</div>
-      )}
+      {renderAuthor(authors)}
     </Container>
   );
 };
--- client/src/feature/blog/BlogList.tsx
+++ client/src/feature/blog/BlogList.tsx
@@ -6,15 +6,17 @@ interface BlogListProps {
   blogs: Blog[];
 }
 
+const renderBlog = (blogs: Blog[]) => {
+  if (blogs.length <= 0) {
+    return <div className="no-data">No blog yet.</div>;
+  }
+
+  return blogs.map((blog) => <ShortBlog key={blog.blogId} blog={blog} />);
+};
+
 const BlogList = ({ blogs }: BlogListProps) => {
   return (
-    <Container className="blog-container py-5">
-      {blogs && blogs.length ? (
-        blogs.map((blog) => <ShortBlog key={blog.blogId} blog={blog} />)
-      ) : (
-        <div className="no-data">No blog yet.</div>
-      )}
-    </Container>
+    <Container className="blog-container py-5">{renderBlog(blogs)}</Container>
   );
 };
 
